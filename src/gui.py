77357285e69202fc0ca2a8cee75e42064244c3bf
operationguy <62300449+operationguy@users.noreply.github.com>
2025-02-12 06:52:25 -0500

    need to fix texture non-rendering

--- src/gui.py
+++ src/gui.py
@@ -8,6 +8,7 @@ import glfw
 import numpy as np
 import tkinter as tk
 from pyopengltk import OpenGLFrame
+from PIL import Image
 
 
 class UI():
@@ -33,10 +34,24 @@ class UI():
 
         # triangle data
         vertices = np.array([
-            -0.5, -0.5, 0.0,  1.0, 0.0, 0.0,
-            0.5, -0.5, 0.0,   0.0, 1.0, 0.0,
-            0.0, 0.5, 0.0,    0.0, 0.0, 1.0
+            -0.5, -0.5, 0.0,  1.0, 0.0, 0.0,  0.0, 0.0,
+            0.5, -0.5, 0.0,   0.0, 1.0, 0.0,  1.0, 0.0,
+            0.0, 0.5, 0.0,    0.0, 0.0, 1.0,  0.5, 1.0
         ], dtype=np.float32)
+
+        # generate & configure texture
+        img = Image.open("container.jpg")
+        img_data = np.array(list(img.getdata()))
+        texture = gl.glGenTextures(1)
+        gl.glBindTexture(gl.GL_TEXTURE_2D, texture)
+
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_S, gl.GL_MIRRORED_REPEAT)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_WRAP_T, gl.GL_MIRRORED_REPEAT)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MIN_FILTER, gl.GL_LINEAR_MIPMAP_LINEAR)
+        gl.glTexParameteri(gl.GL_TEXTURE_2D, gl.GL_TEXTURE_MAG_FILTER, gl.GL_LINEAR)
+
+        gl.glTexImage2D(gl.GL_TEXTURE_2D, 0, gl.GL_RGB, img.width, img.height, 0, gl.GL_RGB, gl.GL_UNSIGNED_BYTE, img_data)
+        gl.glGenerateMipmap(gl.GL_TEXTURE_2D)
 
         # vertex buffer & array object
         self.vao = gl.glGenVertexArrays(1)
@@ -46,11 +61,14 @@ class UI():
         gl.glBindBuffer(gl.GL_ARRAY_BUFFER, vbo)
         gl.glBufferData(gl.GL_ARRAY_BUFFER, vertices.nbytes, vertices, gl.GL_STATIC_DRAW)
 
-        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, False, 6 * 4, null_ptr(0))
+        gl.glVertexAttribPointer(0, 3, gl.GL_FLOAT, False, 8 * 4, null_ptr(0))
         gl.glEnableVertexAttribArray(0)
 
-        gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, False, 6 * 4, null_ptr(3 * 4))
+        gl.glVertexAttribPointer(1, 3, gl.GL_FLOAT, False, 8 * 4, null_ptr(3 * 4))
         gl.glEnableVertexAttribArray(1)
+
+        gl.glVertexAttribPointer(2, 2, gl.GL_FLOAT, False, 8 * 4, null_ptr(6 * 4))
+        gl.glEnableVertexAttribArray(2)
 
         # runtime loop
         while not (glfw.window_should_close(window)):
@@ -65,8 +83,9 @@ class UI():
             gl.glClearColor(0.2, 0.3, 0.3, 1.0)
             gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
+            gl.glBindTexture(gl.GL_TEXTURE_2D, texture)
             gl.glBindVertexArray(self.vao)
-            gl.glDrawArrays(gl.GL_TRIANGLES, 0, 3)
+            gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, 0)
 
             # events & buffer swap
             glfw.swap_buffers(window)
