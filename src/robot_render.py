885b462c8c0bd54e0a528f7a9e204f095015fdc1
operationguy <62300449+operationguy@users.noreply.github.com>
2025-02-15 21:33:04 -0500

    moved tutorial content to separate file, started frameworking GL window for robot rendering

---
+++ src/robot_render.py
@@ -0,0 +1,74 @@
+import shader
+import ctypes
+#import OpenGL.GL as gl
+from OpenGL.GL import *
+import glfw
+import numpy as np
+from PIL import Image
+from pyglm import glm
+
+null_ptr = ctypes.c_void_p
+
+class UI():
+    def __init__(self):
+        self.vao = None
+        self.vbo = None
+        self.ebo = None
+        self.shader = None
+    
+    def main(self):
+        # ======================= #
+        #          SETUP          #
+        # ======================= #
+
+        # Initialize GLFW
+        glfw.init()
+        glfw.window_hint(glfw.CONTEXT_VERSION_MAJOR, 3)
+        glfw.window_hint(glfw.CONTEXT_VERSION_MINOR, 3)
+        glfw.window_hint(glfw.OPENGL_PROFILE, glfw.OPENGL_CORE_PROFILE)
+
+        # Initialize GL Context
+        window = glfw.create_window(800, 600, "Charles Marwin", None, None)
+        glfw.make_context_current(window)
+        glViewport(0,0,800,600)
+        glEnable(GL_DEPTH_TEST)
+
+        # Compile Shader
+        self.shader = shader.Shader("vertex_shader.glsl", "fragment_shader.glsl")
+
+        # Vertex Array initialization
+        self.vao = glGenVertexArrays(1)
+        glBindVertexArray(self.vao)
+
+        # Vertex Buffer initialization
+        self.vbo = glGenBuffers(1)
+        glBindBuffer(GL_ARRAY_BUFFER, self.vbo)
+        #glBufferData(GL_ARRAY_BUFFER, ___, ___, GL_STATIC_DRAW)
+        #glVertexAttribPointer(_index, _size, _type, _normalized, _stride, _offset)
+        #glEnableVertexAttribArray(_index)
+
+        # Element Buffer initialization
+        # NOTICE: If object files do not support vertex indexing, the EBO is unnecessary.
+        self.ebo = glGenBuffers(1)
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, self.ebo)
+        #
+
+        # ======================= #
+        #       RENDER LOOP       #
+        # ======================= #
+        while not (glfw.window_should_close(window)):
+            # Press ESC to exit
+            if (glfw.get_key(window, glfw.KEY_ESCAPE) == glfw.PRESS):
+                glfw.set_window_should_close(window,True)
+
+            # Render stuff here
+            self.shader.use() # NOTICE: If only one shader is in use, can place this in setup.
+            self.shader.set_float("someUniform", 1.0)
+            # events & buffer swap
+            glfw.swap_buffers(window)
+            glfw.poll_events()
+
+        glfw.terminate()
+
+ui_instance = UI()
+ui_instance.main()
