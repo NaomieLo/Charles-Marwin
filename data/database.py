b2cb1ef4ef52e7b2316cbee2286a15e86a6e5bd1
Léanne Ricard <143901674+Lerianne@users.noreply.github.com>
2025-04-02 12:02:47 -0400

    path update in database.py

--- data/database.py
+++ data/database.py
@@ -22,7 +22,7 @@ def authenticate():
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file("data/credentials.json", SCOPES)
+            flow = InstalledAppFlow.from_client_secrets_file("../data/credentials.json", SCOPES)
             creds = flow.run_local_server(port=0, open_browser=False)
 
             #creds = flow.run_local_server(port=0) 
